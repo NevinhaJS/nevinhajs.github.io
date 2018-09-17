10fcbd96144d93b347ccb4c8e9209afb2d32e313
Clóvis Neto <cloves13@hotmail.com>
2018-09-17 13:58:57 -0300

    Adjust google analytics script call

--- electric.config.js
+++ electric.config.js
@@ -7,5 +7,5 @@ module.exports = {
 	sassOptions: {
 		includePaths: ['node_modules', marble.src]
 	},
-	vendorSrc: ['node_modules/marble/build/fonts/**']
+	vendorSrc: ['node_modules/marble/build/fonts/**', 'https://www.googletagmanager.com/gtag/js?id=UA-122807437-1']
 };
